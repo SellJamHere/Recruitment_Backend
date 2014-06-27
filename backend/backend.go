ea842221b56aab36c3439ec1bcc7994cf219bd5b
James Heller <jaheller5@cox.net>
2014-06-27 16:18:47 -0700

    Base backend

--- backend/backend.go
+++ backend/backend.go
@@ -8,10 +8,4 @@ import (
 func init() {
 	// No Favicon this is an API Server. But god forbid you actually use this in a browser.
 	http.Handle("/favicon.ico", http.NotFoundHandler())
-
-	// http.HandleFunc("/", handler)
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello!")
 }
