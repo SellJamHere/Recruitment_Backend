e75f357b4b5b93d86ab4adc597cb0b2dd098c513
James Heller <jaheller5@cox.net>
2014-08-15 16:32:46 -0700

    Add gender support

--- backend/models/recruit.go
+++ backend/models/recruit.go
@@ -10,6 +10,7 @@ type Recruit struct {
 	Email          string    `datastore:"email" json:"email,omitempty"`
 	Year           int       `datastore:"year" json:"year,omitempty"`
 	Major          string    `datastore:"major" json:"major,omitempty"` //Possibly make this a dropdown with enumerated majors
+	IsMale         bool      `datastore:"male" json:"male,omitempty"`
 	DateRegistered time.Time `datastore:"date_registered" json:"date_registered,omitempty"`
 	// interest_level  int
 }
